5b8d170e73e190b39f3a51fb4bf60ab45f8e97c6
ALEXANDRE DA SILVA SOUZA <alexandresouza9@gmail.com>
2022-10-27 15:40:19 -0300

    Commit

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1", "Sobrenome 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2", "Sobrenome 1");
+Pessoa p1 = new Pessoa(nome: "Hóspede 1", "Sobrenome 1 ");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2", "Sobrenome 2 ");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 1, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
 Reserva reserva = new Reserva(diasReservados: 10);
